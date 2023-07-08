4ad1af8199abf8b95f2a143268fbe8b1a1b366da
andallves <andrealves10a@gmail.com>
2023-07-08 14:27:01 -0300

    Update Button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -8,14 +8,14 @@ export const Button = styled.button`
     background: -webkit-linear-gradient(to right, #514a9d, #24c6dc);
     background: linear-gradient(to right, #514a9d, #24c6dc);
     border-radius: 5rem;
-    font-family: ${theme.font.family.default};
+    font-family: ${theme.font.family.title};
     font-style: normal;
     font-weight: 400;
     font-size: ${theme.font.sizes.xsmall};
     line-height: 4.2rem;
     color: ${theme.colors.whiteColor};
+    transition: ${theme.transitions.normal};
     cursor: pointer;
-    transition: all .5s;
 
     &:hover {
       opacity: .8;
